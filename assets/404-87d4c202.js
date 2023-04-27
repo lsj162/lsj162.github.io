905a9fe258b287ef514a0646ff1fac35f11f8810
global name <global email>
2023-04-27 10:17:08 +0800

    fix: 修改

---
+++ assets/404-87d4c202.js
@@ -0,0 +1,1 @@
+import{j as r,F as a}from"./index-d556deb5.js";function t(){return r(a,{children:"404..."})}export{t as default};
--- assets/404-b5f8bbe9.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as a}from"./index-e08c28f3.js";function t(){return r(a,{children:"404..."})}export{t as default};
