4788a43f4ce91bcbb50c34ad2733b1f672a36e44
global name <global email>
2023-09-20 17:33:35 +0800

    fix: 修改

---
+++ assets/工具_其他_hexo_index.md.093efd1c.js
@@ -0,0 +1,14 @@
+import{_ as e,o,c as a,S as t}from"./chunks/framework.2524ba1c.js";const m=JSON.parse('{"title":"🎉 Hexo 搭建和使用","description":"","frontmatter":{},"headers":[],"relativePath":"工具/其他/hexo/index.md","filePath":"工具/其他/hexo/index.md"}'),n={name:"工具/其他/hexo/index.md"},r=t(`<h1 id="🎉-hexo-搭建和使用" tabindex="-1">🎉 Hexo 搭建和使用 <a class="header-anchor" href="#🎉-hexo-搭建和使用" aria-label="Permalink to &quot;🎉 Hexo 搭建和使用&quot;">​</a></h1><p>本地快速、简洁且高效的博客框架 · 超快速度. Node.js 所带来的超快生成速度，让上百个页面在几秒内瞬间完成渲染。</p><div class="tip custom-block"><p class="custom-block-title">推荐</p><p>线上编辑博客，推荐在线 Gitbook；</p></div><h2 id="安装-hexo" tabindex="-1">安装 Hexo <a class="header-anchor" href="#安装-hexo" aria-label="Permalink to &quot;安装 Hexo&quot;">​</a></h2><pre><code>依赖环境：
+nodejs
+
+即可使用 npm 安装 Hexo。
+
+$ npm install -g hexo-cli
+</code></pre><h2 id="配置主题" tabindex="-1">配置主题 <a class="header-anchor" href="#配置主题" aria-label="Permalink to &quot;配置主题&quot;">​</a></h2><pre><code>- themes 下载放置此处
+
+- _config.yml
+    - theme: landscape 设置主题
+</code></pre><h2 id="链接" tabindex="-1">链接 <a class="header-anchor" href="#链接" aria-label="Permalink to &quot;链接&quot;">​</a></h2><pre><code>https://hexo.io/zh-cn/docs/
+
+https://hexo.io/themes/
+</code></pre>`,9),c=[r];function s(i,h,d,l,p,_){return o(),a("div",null,c)}const u=e(n,[["render",s]]);export{m as __pageData,u as default};
---
+++ assets/数据库_mysql_基本语法_函数.md.9ea39073.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.2524ba1c.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"数据库/mysql/基本语法/函数.md","filePath":"数据库/mysql/基本语法/函数.md"}'),s={name:"数据库/mysql/基本语法/函数.md"};function r(o,c,n,_,m,p){return t(),a("div")}const l=e(s,[["render",r]]);export{i as __pageData,l as default};
