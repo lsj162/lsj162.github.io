a2684533d70e30fb0af2ee4dc3e13f63d345ff7c
global name <global email>
2024-03-16 21:43:17 +0800

    fix: 修改

---
+++ assets/前端_CSS_目录_Flex_index.md.Drvtz91z.js
@@ -0,0 +1,21 @@
+import{_ as a,c as n,o as s,a5 as e}from"./chunks/framework.-E9IboRH.js";const x=JSON.parse('{"title":"","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"前端/CSS/目录/Flex/index.md","filePath":"前端/CSS/目录/Flex/index.md"}'),p={name:"前端/CSS/目录/Flex/index.md"},l=e(`<p><a href="/前端/CSS/">返回</a></p><h2 id="flex-api" tabindex="-1">Flex Api <a class="header-anchor" href="#flex-api" aria-label="Permalink to &quot;Flex Api&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>## &quot;容器&quot;（container）</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>flex-flow：简写，默认值为 row nowrap。</span></span>
+<span class="line"><span>flex-direction 轴的方向</span></span>
+<span class="line"><span>flex-wrap 是否换行</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>**对齐方式**</span></span>
+<span class="line"><span>justify-content</span></span>
+<span class="line"><span>align-items</span></span>
+<span class="line"><span>align-content</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>## &quot;项目&quot;（item）</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>flex：简写，默认值为 0 1 auto。</span></span>
+<span class="line"><span>flex-grow 放大比例</span></span>
+<span class="line"><span>flex-shrink 缩小比例</span></span>
+<span class="line"><span>flex-basis</span></span>
+<span class="line"><span>order</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>**对齐方式**</span></span>
+<span class="line"><span>align-self</span></span></code></pre></div><h2 id="参考链接" tabindex="-1">参考链接 <a class="header-anchor" href="#参考链接" aria-label="Permalink to &quot;参考链接&quot;">​</a></h2><p><a href="https://developer.mozilla.org/zh-CN/docs/Web/CSS/flex" target="_blank" rel="noreferrer">MDN 官网：https://developer.mozilla.org/zh-CN/docs/Web/CSS/flex</a></p>`,5),i=[l];function t(o,c,r,d,h,_){return s(),n("div",null,i)}const u=a(p,[["render",t]]);export{x as __pageData,u as default};
