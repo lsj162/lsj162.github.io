169776a29a5dd04482a1fe2dda4614f9346de411
global name <global email>
2024-06-02 15:40:56 +0800

    fix: 修改

---
+++ assets/后端_其他_Nginx_运行_运行.md.2SvkLtsA.js
@@ -0,0 +1,17 @@
+import{_ as s,c as a,o as i,a5 as n}from"./chunks/framework.DALfHeHd.js";const F=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"后端/其他/Nginx/运行/运行.md","filePath":"后端/其他/Nginx/运行/运行.md","lastUpdated":1716891854000}'),p={name:"后端/其他/Nginx/运行/运行.md"},l=n(`<h2 id="启动、停止和重新加载配置" tabindex="-1">启动、停止和重新加载配置 <a class="header-anchor" href="#启动、停止和重新加载配置" aria-label="Permalink to &quot;启动、停止和重新加载配置&quot;">​</a></h2><p>要启动 nginx，请运行可执行文件。一旦 nginx 启动，就可以通过带 <code>-s</code> 参数调用可执行文件来控制它。使用以下常见的语法：</p><div class="language-sh vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 启动</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">nginx</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 关闭</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">nginx</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -s</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> stop</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 重启</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">nginx</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -s</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> reload</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 退出</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">nginx</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -s</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> quit</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 查看版本</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">nginx</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -v</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -V（编译器版本和配置参数）</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 测试配置文件</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">nginx</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -t</span></span></code></pre></div>`,3),t=[l];function e(h,k,d,r,c,g){return i(),a("div",null,t)}const _=s(p,[["render",e]]);export{F as __pageData,_ as default};
