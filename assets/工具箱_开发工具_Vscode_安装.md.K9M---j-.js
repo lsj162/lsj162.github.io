627b9df63005fdb35e0b40c5450fe5d687d6b033
global name <global email>
2024-06-02 17:19:14 +0800

    fix: 修改

---
+++ assets/工具箱_开发工具_Vscode_安装.md.K9M---j-.js
@@ -0,0 +1,11 @@
+import{_ as a,c as s,o as e,a5 as n}from"./chunks/framework.DALfHeHd.js";const m=JSON.parse('{"title":"开发工具","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"工具箱/开发工具/Vscode/安装.md","filePath":"工具箱/开发工具/Vscode/安装.md","lastUpdated":1717317169000}'),t={name:"工具箱/开发工具/Vscode/安装.md"},o=n(`<h1 id="开发工具" tabindex="-1">开发工具 <a class="header-anchor" href="#开发工具" aria-label="Permalink to &quot;开发工具&quot;">​</a></h1><h2 id="vscode" tabindex="-1">vscode <a class="header-anchor" href="#vscode" aria-label="Permalink to &quot;vscode&quot;">​</a></h2><p>目前最佳的前端开发工具</p><h3 id="vscode-插件推荐" tabindex="-1">Vscode 插件推荐 <a class="header-anchor" href="#vscode-插件推荐" aria-label="Permalink to &quot;Vscode 插件推荐&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>- open in browser 在浏览器中打开</span></span>
+<span class="line"><span>- vscode-icons 将真实的图标引入您的 Visual Studio Code</span></span>
+<span class="line"><span>- Better Comments Better Comments 扩展将帮助您在代码中创建更人性化的注释。</span></span>
+<span class="line"><span>- Code Spell Checker 一个基本的拼写检查器，可以很好地处理代码和文档。</span></span>
+<span class="line"><span>- Import Cost 此扩展将在编辑器中内联显示导入包的大小。</span></span>
+<span class="line"><span>- Path Intellisense 路径智能感知，自动完成文件名的 Visual Studio Code 插件。</span></span>
+<span class="line"><span>- Turbo Console Log 此扩展通过自动执行写入有意义的日志消息的操作，使调试变得更加容易。</span></span>
+<span class="line"><span>- Prettier Prettier is an opinionated code formatter.</span></span>
+<span class="line"><span>- Stylelint The official Visual Studio Code extension for Stylelint</span></span>
+<span class="line"><span>- Git Graph</span></span>
+<span class="line"><span>- Git History 查看 git 日志、文件历史记录、比较分支或提交</span></span></code></pre></div>`,5),p=[o];function i(l,c,d,r,h,_){return e(),s("div",null,p)}const f=a(t,[["render",i]]);export{m as __pageData,f as default};
