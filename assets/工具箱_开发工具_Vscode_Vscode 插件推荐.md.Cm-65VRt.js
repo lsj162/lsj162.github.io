1b58f392b60157098e318924ae1438999dab6e47
global name <global email>
2024-07-11 21:35:38 +0800

    fix: 修改

---
+++ assets/工具箱_开发工具_Vscode_Vscode 插件推荐.md.Cm-65VRt.js
@@ -0,0 +1,21 @@
+import{_ as s,c as a,o as i,a5 as n}from"./chunks/framework.DALfHeHd.js";const _=JSON.parse('{"title":"开发工具","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"工具箱/开发工具/Vscode/Vscode 插件推荐.md","filePath":"工具箱/开发工具/Vscode/Vscode 插件推荐.md","lastUpdated":null}'),e={name:"工具箱/开发工具/Vscode/Vscode 插件推荐.md"},t=n(`<h1 id="开发工具" tabindex="-1">开发工具 <a class="header-anchor" href="#开发工具" aria-label="Permalink to &quot;开发工具&quot;">​</a></h1><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>热门的编程开发工具，非常适合前端。</p></div><h3 id="vscode-插件推荐" tabindex="-1">vscode 插件推荐 <a class="header-anchor" href="#vscode-插件推荐" aria-label="Permalink to &quot;vscode 插件推荐&quot;">​</a></h3><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> open in browser 在浏览器中打开</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> vscode-icons 将真实的图标引入您的 Visual Studio Code</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Better Comments Better Comments 扩展将帮助您在代码中创建更人性化的注释。</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Code Spell Checker 一个基本的拼写检查器，可以很好地处理代码和文档。</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Import Cost 此扩展将在编辑器中内联显示导入包的大小。</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Path Intellisense 路径智能感知，自动完成文件名的 Visual Studio Code 插件。</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Turbo Console Log 此扩展通过自动执行写入有意义的日志消息的操作，使调试变得更加容易。</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Prettier Prettier is an opinionated code formatter.</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Stylelint The official Visual Studio Code extension for Stylelint</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Git Graph</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> Git History 查看 git 日志、文件历史记录、比较分支或提交</span></span></code></pre></div><h2 id="参考链接" tabindex="-1">参考链接 <a class="header-anchor" href="#参考链接" aria-label="Permalink to &quot;参考链接&quot;">​</a></h2><p><a href="https://code.visualstudio.com/Download" target="_blank" rel="noreferrer">下载 Visual Studio Code 地址</a></p>`,6),l=[t];function p(h,o,d,r,c,k){return i(),a("div",null,l)}const g=s(e,[["render",p]]);export{_ as __pageData,g as default};
